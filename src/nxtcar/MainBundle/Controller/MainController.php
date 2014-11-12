32a73b596eec0e8a2b1ce8af8698a8ebce035f0b
Hazarapet Tunanyan <hazarapettunanyan@gmail.com>
2014-11-12 22:13:34 +0400

    adding routes

---
+++ src/nxtcar/MainBundle/Controller/MainController.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: hazarapet
+ * Date: 11/12/14
+ * Time: 9:57 PM
+ */
+
+namespace nxtcar\MainBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
+class MainController extends Controller
+{
+    /**
+     * @Route("/")
+     * @Template("MainBundle:Main:index.html.twig")
+     */
+    public function indexAction()
+    {
+        return array();
+    }
+} 
