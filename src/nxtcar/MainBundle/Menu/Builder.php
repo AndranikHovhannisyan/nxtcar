7f271818ec36d5fbe1d57eae5a81ebaa9ea3443f
andranik <An.corp@mail.ru>
2014-11-15 17:49:04 +0400

    css ow menus in progress Aram

--- src/nxtcar/MainBundle/Menu/Builder.php
+++ src/nxtcar/MainBundle/Menu/Builder.php
@@ -25,21 +25,23 @@ class Builder extends ContainerAware
         $request = $this->container->get('request');
         $menu->setCurrentUri($request->getRequestUri());
 
+        $menu->setChildrenAttribute('class', 'list-inline user-profil-menue');
         $menu->addChild('Dashboard', array('route' => 'dashboard'));
-        $menu->addChild('Rides offered', array('route' => ''));
+        $menu->addChild('Rides offered', array('route' => 'homepage'));
         $menu->addChild('Messages', array('route' => 'messages', 'routeParameters' => array('status' => 'received')));
-        $menu->addChild('Ratings', array('route' => ''));
+        $menu->addChild('Ratings', array('route' => 'homepage'));
         $menu->addChild('Profile', array('route' => 'sonata_user_profile_edit'));
 
         $menu['Messages']->addChild('Archived messages', array('route' => 'messages', 'routeParameters' => array('status' => 'questions_answers')));
         $menu['Messages']->addChild('Private messages', array('route' => 'messages', 'routeParameters' => array('status' => 'received')));
         $menu['Messages']->addChild('Public questions', array('route' => 'messages', 'routeParameters' => array('status' => 'archived')));
 
+        $menu['Profile']->setChildrenAttribute('class', 'list-unstyled profile-information');
         $menu['Profile']->addChild('Personal information', array('route' => 'sonata_user_profile_edit'));
-        $menu['Profile']->addChild('Photo', array('route' => ''));
-        $menu['Profile']->addChild('Preferences', array('route' => ''));
-        $menu['Profile']->addChild('Car', array('route' => ''));
-        $menu['Profile']->addChild('Password', array('route' => ''));
+        $menu['Profile']->addChild('Photo', array('route' => 'homepage'));
+        $menu['Profile']->addChild('Preferences', array('route' => 'homepage'));
+        $menu['Profile']->addChild('Car', array('route' => 'homepage'));
+        $menu['Profile']->addChild('Password', array('route' => 'homepage'));
 
 
         return $menu;
