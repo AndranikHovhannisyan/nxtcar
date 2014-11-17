ebe489ac66a42a531d16d35e7b3ae088d4c16ae9
andranik <An.corp@mail.ru>
2014-11-18 00:35:03 +0400

    some changes

--- src/nxtcar/UserBundle/Entity/User.php
+++ src/nxtcar/UserBundle/Entity/User.php
@@ -111,4 +111,12 @@ class User extends BaseUser
 
         return $this;
     }
+
+    public function setEmail($email)
+    {
+        $this->email = $email;
+        $this->username = $email;
+
+        return $this;
+    }
 }
