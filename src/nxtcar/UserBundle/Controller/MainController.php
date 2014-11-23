9032718155b717253e4bd6dd95825d0ccf39ec8d
andranik <An.corp@mail.ru>
2014-11-23 21:25:40 +0400

    preferences controller

--- src/nxtcar/UserBundle/Controller/MainController.php
+++ src/nxtcar/UserBundle/Controller/MainController.php
@@ -8,10 +8,13 @@
 
 namespace nxtcar\UserBundle\Controller;
 
+use FOS\RestBundle\Util\Codes;
 use nxtcar\UserBundle\Entity\User;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Exception\HttpException;
 
 class MainController extends Controller
 {
@@ -19,9 +22,47 @@ class MainController extends Controller
      * @Route("/profile/preferences", name="profile_preferences")
      * @Template("nxtcarUserBundle:Profile:preferences.html.twig")
      */
-    public function preferencesAction()
+    public function preferencesAction(Request $request)
     {
-        return array();
+        $user = $this->get('security.context')->getToken()->getUser();
+
+        if ($request->getMethod() == "POST" && $request->get('preferences') != "") {
+
+            $obj = json_decode($request->get('preferences'));
+
+            if (isset($obj->chattiness) && $obj->chattiness >= 0 && $obj->chattiness <= 2) {
+                $user->setChattiness($obj->chattiness);
+            }
+            else {
+                throw new HttpException(Codes::HTTP_BAD_REQUEST);
+            }
+
+            if (isset($obj->music) && $obj->music >= 0 && $obj->music <= 2) {
+                $user->setMusic($obj->music);
+            }
+            else {
+                throw new HttpException(Codes::HTTP_BAD_REQUEST);
+            }
+
+            if (isset($obj->smoking) && $obj->smoking >= 0 && $obj->smoking <= 2) {
+                $user->setSmoking($obj->smoking);
+            }
+            else {
+                throw new HttpException(Codes::HTTP_BAD_REQUEST);
+            }
+
+            if (isset($obj->pets) && $obj->pets >= 0 && $obj->pets <= 2) {
+                $user->setPets($obj->pets);
+            }
+            else {
+                throw new HttpException(Codes::HTTP_BAD_REQUEST);
+            }
+
+            $em = $this->getDoctrine()->getManager();
+            $em->flush();
+        }
+
+        return array('user' => $user);
     }
 
     /**
