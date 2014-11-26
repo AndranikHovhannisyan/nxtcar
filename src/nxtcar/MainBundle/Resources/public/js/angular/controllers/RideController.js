c0764b8b625785c7567b8db970ec492ab21519ef
Hazarapet Tunanyan <hazarapettunanyan@gmail.com>
2014-11-26 23:12:51 +0400

    sortable cities,add city

--- src/nxtcar/MainBundle/Resources/public/js/angular/controllers/RideController.js
+++ src/nxtcar/MainBundle/Resources/public/js/angular/controllers/RideController.js
@@ -4,5 +4,13 @@ define([],function(){
     return angular.module('Ride')
         .controller('RideController',function($scope){
             $scope.cities = [{name: ""}];
+
+            /*-----------------------------*/
+            $scope.removeCity = function(index){
+                if(angular.isUndefined(index) || !angular.isNumber(index)) {
+                    return;
+                }
+                $scope.cities.splice(index,1);
+            }
         });
 })
