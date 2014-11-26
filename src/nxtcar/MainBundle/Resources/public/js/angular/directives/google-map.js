44e3a2abf07a08098f963e807c6f692f309b7f27
Hazarapet Tunanyan <hazarapettunanyan@gmail.com>
2014-11-26 22:14:38 +0400

    added google map

--- src/nxtcar/MainBundle/Resources/public/js/angular/directives/google-map.js
+++ src/nxtcar/MainBundle/Resources/public/js/angular/directives/google-map.js
@@ -3,12 +3,20 @@
 define([],function(){
     return angular.module('Ride')
         .directive('googleMap',function(){
+            function Initialize(el,zoom){
+                var m,data = {};
+                data.center = new google.maps.LatLng(54.059388,-1.5);
+                data.mapTypeId = google.maps.MapTypeId.ROADMAP;
+                data.zoom = zoom;
+                m = new google.maps.Map(el,data);
+                return m;
+            };
             return {
                 restrict: 'EA',
                 scope: {},
                 compile: function compileFn(){
-                    return function linkFn(scope,el,attrs){
-
+                    return function linkFn(scope,el){
+                        scope.map = Initialize(el[0],5);
                     }
                 }
             }
