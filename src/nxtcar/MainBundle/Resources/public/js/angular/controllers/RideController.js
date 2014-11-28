57697b1b286e4d6e186b5836f7c3412431673f68
Hazarapet Tunanyan <hazarapettunanyan@gmail.com>
2014-11-29 00:05:39 +0400

    corrections

--- src/nxtcar/MainBundle/Resources/public/js/angular/controllers/RideController.js
+++ src/nxtcar/MainBundle/Resources/public/js/angular/controllers/RideController.js
@@ -2,10 +2,13 @@
 
 define([],function(){
     return angular.module('Ride')
-        .controller('RideController',function($scope){
+        .controller('RideController',function($scope,countries){
             $scope.cities = [{name: ""}];
             $scope.minutes = ['00','10','20','30','40','50'];
             $scope.hours = [];
+            $scope.countries = angular.copy(countries);
+            $scope.choosenCountry = $scope.countries.gb;
+
 
             for(var i = 0; i < 24; i++){
                 if(i < 10){
@@ -16,6 +19,13 @@ define([],function(){
                 }
             }
 
+            /*-----------------------------*/
+            $scope.$watch('choosenCountry',function(d){
+                if(angular.isUndefined(d)){
+                    return;
+                }
+                angular.element('.currency').html(d.currency);
+            },true);
             /*-----------------------------*/
             $scope.removeCity = function(index){
                 if(angular.isUndefined(index) || !angular.isNumber(index)) {
