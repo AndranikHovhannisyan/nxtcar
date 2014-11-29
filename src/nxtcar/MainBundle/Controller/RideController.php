3a68da014f1b14157194f8525527304822cd1ee0
andranik <An.corp@mail.ru>
2014-11-30 00:46:20 +0400

    add comments

--- src/nxtcar/MainBundle/Controller/RideController.php
+++ src/nxtcar/MainBundle/Controller/RideController.php
@@ -42,12 +42,12 @@ class RideController extends Controller
     }
 
     /**
-     * @Route("/ride/{id}", name="ride_show")
+     * @Route("/ride/{rideId}", name="ride_show")
      * @Template()
      */
-    public function publicMessagesAction($id, Request $request)
+    public function publicMessagesAction($rideId, Request $request)
     {
-        $id = 'ride_' . $id;
+        $id = 'ride_' . $rideId;
         $thread = $this->container->get('fos_comment.manager.thread')->findThreadById($id);
         if (null === $thread) {
             $thread = $this->container->get('fos_comment.manager.thread')->createThread();
@@ -58,11 +58,68 @@ class RideController extends Controller
             $this->container->get('fos_comment.manager.thread')->saveThread($thread);
         }
 
+        $comment = $this->container->get('fos_comment.manager.comment')->createComment($thread);
+
+        $form = $this->createFormBuilder($comment)
+            ->setAction('#')
+            ->add('body')
+            ->add('submit', 'submit')
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isValid())
+        {
+            $manager = $this->container->get('fos_comment.manager.comment');
+            $manager->saveComment($comment);
+
+            return $this->redirect($this->generateUrl('ride_show', array('rideId' => $rideId)));
+        }
+
         $comments = $this->container->get('fos_comment.manager.comment')->findCommentTreeByThread($thread);
 
         return $this->render('nxtcarMainBundle:Ride:ride.html.twig', array(
             'comments' => $comments,
             'thread' => $thread,
+            'form' => $form->createView()
         ));
+    }
+
+    /**
+     * @param $id
+     * @return mixed
+     * @throws NotFoundHttpException
+     * @Route("/ride/comment/save/{id}", name="ride_comment_save")
+     * @Template()
+     */
+    public function newThreadCommentsAction($id)
+    {
+        $thread = $this->container->get('fos_comment.manager.thread')->findThreadById($id);
+        if (!$thread) {
+            throw new NotFoundHttpException(sprintf('Thread with identifier of "%s" does not exist', $id));
+        }
+
+        $comment = $this->container->get('fos_comment.manager.comment')->createComment($thread);
+
+//        $parent = $this->getValidCommentParent($thread, $this->getRequest()->query->get('parentId'));
+
+        $form = $this->container->get('fos_comment.form_factory.comment')->createForm();
+        $form->setData($comment);
+
+        return $this->redirect($this->generateUrl('ride_show', array('id' => 1)));
+
+//
+//
+//        $view = View::create()
+//            ->setData(array(
+//                'form' => $form->createView(),
+//                'first' => 0 === $thread->getNumComments(),
+//                'thread' => $thread,
+//                'parent' => $parent,
+//                'id' => $id,
+//            ))
+//            ->setTemplate(new TemplateReference('FOSCommentBundle', 'Thread', 'comment_new'));
+//
+//        return $this->getViewHandler()->handle($view);
     }
 }
