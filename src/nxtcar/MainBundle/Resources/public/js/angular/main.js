593a2ff70960fc4d0e8b338117e7698dcc73c484
Hazarapet Tunanyan <hazarapettunanyan@gmail.com>
2014-12-01 23:45:00 +0400

    correction

--- src/nxtcar/MainBundle/Resources/public/js/angular/main.js
+++ src/nxtcar/MainBundle/Resources/public/js/angular/main.js
@@ -7,23 +7,23 @@ define([],function(){
         'Interpolation',
         'mgcrea.ngStrap.popover',
         'ngAnimate'])
-    })
-    .directive("currency",function(){
-        return {
-            restrict: 'A',
-            scope: {
-                currency: '='
-            },
-            compile: function(){
-                return function(scope,el){
-                    scope.$watch('currency',function(d){
-                        if(angular.isUndefined(d)){
-                            return;
+            .directive("currency",function(){
+                return {
+                    restrict: 'A',
+                    scope: {
+                        currency: '='
+                    },
+                    compile: function(){
+                        return function(scope,el){
+                            scope.$watch('currency',function(d){
+                                if(angular.isUndefined(d)){
+                                    return;
+                                }
+                                console.log(d,'dd');
+                                el.html(d);
+                            },true)
                         }
-                        console.log(d,'dd');
-                        el.html(d);
-                    },true)
+                    }
                 }
-            }
-        }
-    })
+            })
+})
