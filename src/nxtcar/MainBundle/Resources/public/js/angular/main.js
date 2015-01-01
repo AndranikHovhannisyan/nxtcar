7c1c4eb1ac4e0eb26dc983c61e3e32ab63e63f51
Hazarapet Tunanyan <hazarapettunanyan@gmail.com>
2015-01-01 20:31:28 +0400

    google-autocomplete in index

--- src/nxtcar/MainBundle/Resources/public/js/angular/main.js
+++ src/nxtcar/MainBundle/Resources/public/js/angular/main.js
@@ -25,4 +25,46 @@ define([],function(){
                 }
             }
         })
+        .directive('preventDefault', [function () {
+            return {
+                restrict: 'A',
+                scope: false,
+                compile: function compileFn(){
+                    return function linkFn(scope,el){
+                        el.on("keypress",function(e){
+                            var code = e.keyCode || e.which;
+                            if (code === 13) {
+                                e.preventDefault();
+                                return false;
+                            }
+                        });
+                    };
+                }
+            };
+        }])
+        .directive('googleAutocomplete',function(){
+            return {
+                restrict: 'EA',
+                scope: {
+                    place: '='
+                },
+                compile: function(){
+                    return function(scope,el){
+                        scope.autocomplete = new google.maps.places.Autocomplete(el[0],{types: ['(cities)']});
+
+                        google.maps.event.addListener(scope.autocomplete, 'place_changed', function(){
+                            var place = scope.autocomplete.getPlace();
+                            scope.place = {
+                                formatted_name: place.formatted_address,
+                                location: place.geometry.location,
+                                city_name: place.name,
+                                placeType: scope.placeType,
+                                stopover: true
+                            }
+                            scope.$apply();
+                        })
+                    }
+                }
+            }
+        })
 })
