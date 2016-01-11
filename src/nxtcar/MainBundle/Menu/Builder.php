b268912f59dd4dada40e81b33b2b493e38cfad10
andranik <Andranik.Hovhan@gmail.com>
2016-01-11 11:35:50 +0400

    translations

--- src/nxtcar/MainBundle/Menu/Builder.php
+++ src/nxtcar/MainBundle/Menu/Builder.php
@@ -23,30 +23,30 @@ class Builder extends ContainerAware
         $menu = $factory->createItem('root');
 
         $request = $this->container->get('request');
+        $trans = $this->container->get('translator');
         $menu->setCurrentUri($request->getRequestUri());
 
         $menu->setChildrenAttribute('class', 'list-inline user-profil-menue');
-        $menu->addChild('Dashboard', array('route' => 'dashboard'));
-        $menu->addChild('Rides offered', array('route' => 'ride_offered', 'routeParameters' => array('status' => 'upcoming')));
-        $menu->addChild('Messages', array('route' => 'messages', 'routeParameters' => array('status' => 'received')));
-//        $menu->addChild('Ratings', array('route' => 'homepage'));
-        $menu->addChild('Profile', array('route' => 'sonata_user_profile_edit'));
+        $menu->addChild('Dashboard', array('label' => $trans->trans('base.dashboard', array(), 'dashboard'), 'route' => 'dashboard'));
+        $menu->addChild('Rides offered', array('label' => $trans->trans('base.rides_offered', array(), 'dashboard'), 'route' => 'ride_offered', 'routeParameters' => array('status' => 'upcoming')));
+        $menu->addChild('Messages', array('label' => $trans->trans('base.messages', array(), 'dashboard'), 'route' => 'messages', 'routeParameters' => array('status' => 'received')));
+        $menu->addChild('Profile', array('label' => $trans->trans('base.profile', array(), 'dashboard'), 'route' => 'sonata_user_profile_edit'));
 
         $menu['Messages']->setChildrenAttribute('class', 'ul-message');
-        $menu['Messages']->addChild('Public questions', array('route' => 'messages', 'routeParameters' => array('status' => 'questions_answers')));
-        $menu['Messages']->addChild('Private messages', array('route' => 'messages', 'routeParameters' => array('status' => 'received')));
+        $menu['Messages']->addChild($trans->trans('base.public_questions', array(), 'dashboard'), array('route' => 'messages', 'routeParameters' => array('status' => 'questions_answers')));
+        $menu['Messages']->addChild($trans->trans('base.private_messages', array(), 'dashboard'), array('route' => 'messages', 'routeParameters' => array('status' => 'received')));
 
         $menu['Rides offered']->setChildrenAttribute('class', 'ul-message');
-        $menu['Rides offered']->addChild('Upcoming rides',  array('route' => 'ride_offered', 'routeParameters' => array('status' => 'upcoming')));
-        $menu['Rides offered']->addChild('Past rides', array('route' => 'ride_offered', 'routeParameters' => array('status' => 'past')));
+        $menu['Rides offered']->addChild($trans->trans('base.upcoming_rides', array(), 'dashboard'),  array('route' => 'ride_offered', 'routeParameters' => array('status' => 'upcoming')));
+        $menu['Rides offered']->addChild($trans->trans('base.past_rides', array(), 'dashboard'), array('route' => 'ride_offered', 'routeParameters' => array('status' => 'past')));
 
 
         $menu['Profile']->setChildrenAttribute('class', 'list-unstyled profile-information');
-        $menu['Profile']->addChild('Personal information', array('route' => 'sonata_user_profile_edit'));
-        $menu['Profile']->addChild('Photo', array('route' => 'profile_photo'));
-        $menu['Profile']->addChild('Preferences', array('route' => 'profile_preferences'));
-        $menu['Profile']->addChild('Car', array('route' => 'car_list'));
-        $menu['Profile']->addChild('Password', array('route' => 'sonata_user_change_password'));
+        $menu['Profile']->addChild($trans->trans('base.personal_information', array(), 'dashboard'), array('route' => 'sonata_user_profile_edit'));
+        $menu['Profile']->addChild($trans->trans('base.photo', array(), 'dashboard'), array('route' => 'profile_photo'));
+        $menu['Profile']->addChild($trans->trans('base.preferences', array(), 'dashboard'), array('route' => 'profile_preferences'));
+        $menu['Profile']->addChild($trans->trans('base.car', array(), 'dashboard'), array('route' => 'car_list'));
+        $menu['Profile']->addChild($trans->trans('base.password', array(), 'dashboard'), array('route' => 'sonata_user_change_password'));
 
         return $menu;
     }
